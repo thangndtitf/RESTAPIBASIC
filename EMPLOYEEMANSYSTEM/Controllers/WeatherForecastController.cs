b79d78b5bd5f50988238fd887e41d05cec31441a
thangndtitf <thangndtitf@gmail.com>
2023-02-19 19:36:23 +0700

    First Commit at 19-2-2023

---
+++ EMPLOYEEMANSYSTEM/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using EMPLOYEEMANSYSTEM.ML;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMPLOYEEMANSYSTEM.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static List<Employee> employees = new List<Employee>()
+    {
+        new Employee(){employeeID= 1, employeeName = "Nguyễn Đinh Tất Thắng", DOB = DateTime.Now, employeeNum= 162880},
+        new Employee(){employeeID= 2, employeeName = "Trần NGuyễn Thảo Khanh", DOB = DateTime.Now, employeeNum= 162881},
+
+    };
+
+    [HttpGet]
+    public IEnumerable<Employee> getEmployee()
+    {
+        return employees;
+    }
+
+    [HttpPost]
+    public IEnumerable<Employee> addEmployee([FromBody] Employee emp)
+    {
+        employees.Add(emp);
+        return employees;
+    }
+
+    [HttpPut("{id}")]
+    public IEnumerable<Employee> updateEmployee(int id, [FromBody] Employee emp)
+    {
+        employees[id] = emp;
+        return employees;
+    }
+
+}
+
---
+++ EMPLOYEEMANSYSTEM/ML/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace EMPLOYEEMANSYSTEM.ML
+{
+	public class Employee
+	{
+		public int employeeID { get; set; }
+		public String employeeName { get; set; }
+		public DateTime DOB { get; set; }
+		public int employeeNum { get; set; }
+
+        public Employee(int employeeID, string employeeName, DateTime dob, int employeeNum)
+        {
+            this.employeeID = employeeID;
+            this.employeeName = employeeName ?? throw new ArgumentNullException(nameof(employeeName));
+            DOB = dob;
+            this.employeeNum = employeeNum;
+        }
+
+        public Employee()
+        {
+        }
+    }
+}
+
