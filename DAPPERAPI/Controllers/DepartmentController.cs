2fa5cf1d81e7cc169b353643bc41db484f537a49
thangndtitf <thangndtitf@gmail.com>
2023-03-09 00:25:55 +0700

    Commit Api get All Department in Dapper project

---
+++ DAPPERAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using DAPPERAPI.ML;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DAPPERAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public DepartmentController(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Department>>> getAllDepartment()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var department = await connection.QueryAsync<Department>("select * from MD_DEPARTMENTS");
+            return Ok(department);
+        }
+
+    }
+}
+
---
+++ DAPPERAPI/ML/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DAPPERAPI.ML
+{
+	public class Department
+	{
+		public Department(){}
+		public int departmentID { get; set; } 
+		public string departmentName { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+    }
+	
+}
+
